35391bac75dab64c0c23208f6750bc3a04316e8b
rakhargo <117525800+rakhargo@users.noreply.github.com>
2024-02-23 00:17:15 +0700

    janji cpp

--- cpp/Clothing.cpp
+++ cpp/Clothing.cpp
@@ -1,3 +1,11 @@
+/*Saya Rakha Dhifiargo Hariadi
+ NIM 2209489 mengerjakan soal 
+ Latihan praktikum 2 dalam mata
+ kuliah DPBO
+ untuk keberkahanNya maka saya tidak
+ melakukan kecurangan seperti 
+ yang telah dispesifikasikan. Aamiin.*/ 
+ 
 #include <bits/stdc++.h>
 #include "Product.cpp" // include file class parent
 
